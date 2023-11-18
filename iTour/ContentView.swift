734f56771a20899f2c7274346a744042c582c074
Joshua Buhler <josh@buhler.me>
2023-11-18 06:58:43 -0700

    Display list and create samples button

--- iTour/ContentView.swift
+++ iTour/ContentView.swift
@@ -6,16 +6,41 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    
+    @Environment(\.modelContext) var modelContext
+    
+    @Query var destinations:[Destination]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(destinations) { destination in
+                    VStack(alignment: .leading) {
+                        Text(destination.name)
+                            .font(.headline)
+                        Text(destination.date.formatted(date: .long,
+                                                        time: .shortened))
+                    }
+                }
+            }
+            .navigationTitle("iTour")
+            .toolbar {
+                Button("Add Samples", action: addSamples)
+            }
         }
-        .padding()
+    }
+    
+    func addSamples () {
+        let mustafar = Destination(name: "Mustafar")
+        let scarif = Destination(name: "Scarif")
+        let naboo = Destination(name: "Naboo")
+        
+        modelContext.insert(mustafar)
+        modelContext.insert(scarif)
+        modelContext.insert(naboo)
     }
 }
 
