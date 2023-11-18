58b5af89106dbaabca8a581ef2e1419ae9505076
Joshua Buhler <josh@buhler.me>
2023-11-18 06:48:08 -0700

    Adding Destination.swift

---
+++ iTour/Destination.swift
@@ -0,0 +1,25 @@
+//
+//  Destination.swift
+//  iTour
+//
+//  Created by Joshua Buhler on 11/18/23.
+//
+
+import Foundation
+import SwiftData
+
+
+@Model
+class Destination {
+    var name:String
+    var details:String
+    var date:Date
+    var priority:Int
+    
+    init(name: String = "", details: String = "", date: Date = .now, priority: Int = 2) {
+        self.name = name
+        self.details = details
+        self.date = date
+        self.priority = priority
+    }
+}
